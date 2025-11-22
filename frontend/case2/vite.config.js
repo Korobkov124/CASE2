54df89b40a7495a37c63584c562b06241deef7d0
Korobkov124 <andreyka.korobkov.97@mail.ru>
2025-11-22 11:52:30 +0700

    added styles for homepage

--- frontend/case2/vite.config.js
+++ frontend/case2/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()], 
+  server: {
+    https: true,
+    host: 'localhost',
+    port: 3000
+  }
 })
